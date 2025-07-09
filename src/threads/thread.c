7e6e0d08044802251a47d926e1acd25916dfc577
hkmaeng <hkmaeng9075@naver.com>
2025-07-09 13:39:23 +0900

    "feat:week2 update_next_tick_to_awake 수정"

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -316,7 +316,7 @@ thread_sleep(int64_t ticks)
     cur->tick_to_awake = ticks;
     list_push_back(&blocked_list, &cur->elem);
   }
-  update_next_tick_to_awake();
+  update_next_tick_to_awake(next_tick_to_awake);
 
   cur->status = THREAD_BLOCKED;
 
@@ -353,7 +353,7 @@ thread_awake(int64_t ticks)
     }
   }
 
-  update_next_tick_to_awake();
+  update_next_tick_to_awake(next_tick_to_awake);
 }
 
 /* Yields the CPU.  The current thread is not put to sleep and
@@ -645,15 +645,12 @@ update_next_tick_to_awake (void)
 {
   struct list_elem *e;
 
-  if(list_empty(&blocked_list)){
-    next_tick_to_awake = INT64_MAX;
-  }
-  else{
-      for(e = list_begin(&blocked_list); e != list_end(&blocked_list); e = list_next(e)){
-          struct thread *t = list_entry(e, struct thread, elem);
-          if(t->tick_to_awake < next_tick_to_awake)
-            next_tick_to_awake = t->tick_to_awake;
-      }
+  next_tick_to_awake = INT64_MAX;
+
+  for(e = list_begin(&blocked_list); e != list_end(&blocked_list); e = list_next(e)){
+    struct thread *t = list_entry(e, struct thread, elem);
+    if(t->tick_to_awake < next_tick_to_awake)
+      next_tick_to_awake = t->tick_to_awake;
   }
 }
 
